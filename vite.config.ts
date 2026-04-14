3cff58cbaa7584816dda6e31f43025b3422390b7
Fode Oulare <foulare@e2r8p7.1337.ma>
2026-04-14 02:59:47 +0100

    fix some errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc"; // ← only change
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
