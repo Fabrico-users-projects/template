6224e81f24849f6b7fe3b1961955a58533ad57ab
Fode Oulare <foulare@e2r8p7.1337.ma>
2026-04-15 01:12:43 +0100

    improve the template

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc"; // ← only change
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 export default defineConfig({
 	plugins: [tailwindcss(), react(), cloudflare()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src/react-app"),
+		},
+	},
 });
