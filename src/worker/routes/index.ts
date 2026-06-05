e8f4e379830574320fa5353fb6d164d2a24dd7b8
oularefodos <oularemessie1094@gmail.com>
2026-06-05 20:54:18 +0100

    remove hello routes

--- src/worker/routes/index.ts
+++ src/worker/routes/index.ts
@@ -1,8 +1,5 @@
 import { OpenAPIHono } from '@hono/zod-openapi'
-import hello from './hello'
 
 const routes = new OpenAPIHono<{ Bindings: Env }>()
-
-routes.route('/hello-Fabrico', hello)
 
 export { routes }
